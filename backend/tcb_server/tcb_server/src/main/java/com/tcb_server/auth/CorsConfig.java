d43775b1160ad0cec8bfbda91869de202c150ae1
wonted79 <lgs000423@gmail.com>
2026-03-08 20:55:56 +0900

    change for cors

---
+++ backend/tcb_server/tcb_server/src/main/java/com/tcb_server/auth/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.tcb_server.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**") // 모든 API 허용
+                        .allowedOrigins(
+                                "http://localhost:5173", // 개발 React
+                                "https://tenniscoachbookingsystem:5173"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
